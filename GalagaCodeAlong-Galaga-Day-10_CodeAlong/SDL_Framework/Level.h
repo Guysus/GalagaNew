86e489a1ceeb77d05ca323763d5fe8f3ddf5d26a
Guysus <137240103+Guysus@users.noreply.github.com>
2024-12-12 20:10:06 -0500

    adding xml

--- GalagaCodeAlong-Galaga-Day-10_CodeAlong/SDL_Framework/Level.h
+++ GalagaCodeAlong-Galaga-Day-10_CodeAlong/SDL_Framework/Level.h
@@ -5,8 +5,10 @@
 #include "Butterfly.h"
 #include "Wasp.h"
 #include "Boss.h"
+#include "tinyxml2.h"
 
 using namespace SDLFramework;
+using namespace tinyxml2;
 
 class Level : public GameEntity {
 public:
@@ -28,16 +30,29 @@ private:
 	Player* mPlayer;
 	Formation* mFormation;
 
-	const int MAX_BUTTERFLIES = 16;
-	const int MAX_WASPS = 20;
-	const int MAX_BOSSES = 4;
+	static const int MAX_BUTTERFLIES = 16;
+	static const int MAX_WASPS = 20;
+	static const int MAX_BOSSES = 4;
 
 	int mButterflyCount;
 	int mWaspCount;
 	int mBossCount;
-	std::vector<Enemy*> mEnemies;
+	std::vector<Enemy*> mEnemies;//Holds all the debug keys
+	
+	Butterfly* mFormationButterflies[MAX_BUTTERFLIES];
+	Wasp* mFormationWasp[MAX_WASPS];
+	Boss* mFormationBoss[MAX_BOSSES];
+
+	XMLDocument mSpawningPatterns;
+	int mCurrentFlyInPriority;
+	int mCurrentFlyInIdex;
+
+	float mSpawnDelay;
+	float mSpawnTimer;
+	bool mSpawningFinished;
 
 	int mStage;
+	bool mChallengeStage;
 	bool mStageStarted;
 
 	Texture* mReadyLabel;
